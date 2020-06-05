8ebb4fbd750f15be788a0319195a60b64a50a719
Hadi Issa <hadi@localhost.localdomain>
2020-06-05 18:45:14 +1000

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <v-img v-if="showImage" width="100" src="@/assets/images/image1.jpeg"></v-img>
 
     <v-btn dark x-large @click="showHideImage" block :color="color1">{{name}}</v-btn>
+    
   </v-container>
 </template>
 
@@ -14,16 +15,16 @@ export default {
       if (this.showImage == true) {
         this.showImage = false;
         this.name="Show Image"
-        this.color1="green"
+        this.color1="blue"
       } else {
         this.showImage = true;
         this.name="Hide Image"
-        this.color1="red"
+        this.color1="orange"
       }
     }
  },
   data: () => ({
-    color1: "green",
+    color1: "blue",
     showImage: false,
     name: "Show Image"
   })
